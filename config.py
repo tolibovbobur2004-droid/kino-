54966a145c734f498dec8323ffc4bc9bb5922afd
tolibovbobur2004-droid <tolibovbobur2004@gmail.com>
2026-09-24 09:09:37 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8882722071:AAEhcUufwm4pkYeYcOcKNzx4CZ9euJVJ4ck")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "1077229628"))
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://baza_vcxg_user:c5kR60ueT0VDgBU65v08HpMo7eJgg5Or@dpg-daqa4c49v7es73cdvmcg-a.ohio-postgres.render.com/baza_vcxg")
